841c348ebd427e63a0629ea48c7dbd8908b783ea
t.setoguchi <t.setoguchi2611@gmail.com>
2026-03-06 21:21:52 +0900

    feat:シミュレーション機能追加(#17close)

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def load_rules(config_path):
                 sys.exit(1)
             
         return rules
-            
+          
     except json.JSONDecodeError as e:    
         print(f"[error] {config_path}設定ファイルの形式が正しくありません。")
         sys.exit(1)
@@ -34,11 +34,14 @@ rules = load_rules(config_file)
 
 target_dir_str = rules.get("target_directory", "~")
 target_path = Path(target_dir_str).expanduser()
-print(f"DEBUG: 実際に中身を見に行っているパス -> {target_path.absolute()}")
 
 if not target_path.exists() or not target_path.is_dir():
     print(f"[error] ターゲットディレクトリが見つかりません。: {target_dir_str}")
     sys.exit(1)
+
+is_dry_run = rules.get("dry_run", False)
+if is_dry_run:
+    print("シュミレーションモードです。実際にファイル操作はされません。")
 
 
 sort_map = {}
@@ -61,51 +64,67 @@ log_path = log_folder / f"{today}_sort.log"
 
 with open(log_path, "a", encoding="utf-8") as f:
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S ")
-    log_start = f"{now}:>>>>>>>>>>>移動を開始します\n"
-    f.write(f"{log_start}")
+    log_start = f">>>>>>>>>>>移動を開始します\n"
+    if is_dry_run:
+        f.write(f"{now}:[シミュレーション]{log_start}")
+    
+    else:
+        f.write(f"{log_start}")
 
     for item in list(target_path.iterdir()):
+        if item.is_dir():
+            continue
 
         if item.is_file():
-            print(f"CHECK: {item.name} (拡張子: {item.suffix})")
             ext = item.suffix
             if ext in sort_map:
-                folder_name = sort_map[ext]
-                
+                folder_name = sort_map[ext]                 
                 dest_dir = target_path / folder_name
-                dest_dir.mkdir(parents=True, exist_ok=True)
                 
-                dest_path = dest_dir / item.name
-                copy_count = 1 
-                # 重複チェック
-                while dest_path.exists():
-                    dest_path = dest_dir / f"{item.stem}({copy_count}){item.suffix}"
-                    copy_count += 1
+                if is_dry_run:
+                        print(f"[シミュレーション]移動予定: {target_path} -> {folder_name}")
 
-                
-                try:
-                    # 移動処理の実行            
-                    item.rename(dest_path)
+                else:
+                    dest_dir.mkdir(parents=True, exist_ok=True)
+                    dest_path = dest_dir / item.name
+                    copy_count = 1 
+                    # 重複チェック
+                    while dest_path.exists():
+                        dest_path = dest_dir / f"{item.stem}({copy_count}){item.suffix}"
+                        copy_count += 1
 
-                except Exception as e:
-                    print(f"[error] {item.name} の移動ができませんでした。：{e}")
-                    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S ")
-                    log_err = f"[error] {item.name} の移動ができませんでした。：{e}"
-                    f.write(f"{now}:{log_err}\n")
-                    continue
+                    try:
+                        # 移動処理の実行            
+                        item.rename(dest_path)
+                    
+                    except Exception as e:
+                        print(f"[error] {item.name} の移動ができませんでした。：{e}")
+                        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S ")
+                        log_err = f"[error] {item.name} の移動ができませんでした。：{e}"
+                        f.write(f"{now}:{log_err}\n")
+                        continue
 
-        
-                print(f"移動完了: {dest_dir} -> {folder_name}")
-                report[folder_name] = report.get(folder_name, 0)
-                report[folder_name] += 1
+                    print(f"移動完了: {dest_dir} -> {folder_name}")
+
+                report[folder_name] = report.get(folder_name, 0) + 1
 
     for folder, count in report.items():
-        print(f"フォルダ名：{folder}/カウント回数：{count}回")
-        
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S ")
         log_msg = f"[移動完了] フォルダ名：{folder} に {count}回 移動しました"
-        f.write(f"{now}:{log_msg}\n")
+
+        if is_dry_run:
+            print(f"[シミュレーション]フォルダ名：{folder}/カウント回数：{count}回")
+            f.write(f"{now}[シミュレーション]{log_msg}\n")
+        else:
+            print(f"フォルダ名：{folder}/カウント回数：{count}回")
+            f.write(f"{now}:{log_msg}\n")
+            
 
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S ")
     log_comp = ">>>>>>>>>>>[処理完了]"
-    f.write(f"{now}:{log_comp}\n\n")
+    if is_dry_run:
+        print(f"[シミュレーション]{log_comp}")
+        f.write(f"{now}:[シミュレーション]{log_comp}\n\n")
+    else:
+        print(f"{log_comp}")
+        f.write(f"{now}:{log_comp}\n\n")
